c4d98b033ab150b5fa4759ee09585a4c0992d029
BaoChengyi <bcygswspring168@gmail.com>
2021-10-18 02:28:32 +0800

    第一次提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 忽略函数名和函数体起始大括号之间的空格
+    'space-before-function-paren': 0,
   },
 };
